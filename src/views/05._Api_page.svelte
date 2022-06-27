e530c2baf35e63b9f5fa030d302cb5d9cb1cfa1d
OldCoachman <thedarkknight.vanerk@gmail.com>
2022-06-27 12:10:23 +0300

    test github work

--- src/views/05._Api_page.svelte
+++ src/views/05._Api_page.svelte
@@ -6,7 +6,7 @@
 <hr style="margin-left:12px; margin-right:12px; margin-top: 10px">
 <div class="app">
     <div class="header">
-        <h1>Dog photos</h1>
+        <h2>Dog photos</h2>
         <div id="breed"></div>
     </div>
     <hr style="margin: 0 12px 12px;">
